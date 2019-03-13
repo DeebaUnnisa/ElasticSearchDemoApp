fa9251566cf62e1328756d1dc8f4f69a8a01cacc
U6072725 <U6072725@U6072725-TPD-A>
2019-03-13 16:07:25 +0530

    removed comments

--- ElasticSearchDemoApp/Infrastructure/QueryRepository.cs
+++ ElasticSearchDemoApp/Infrastructure/QueryRepository.cs
@@ -13,17 +13,6 @@ namespace ElasticSearchDemoApp.Infrastructure
     public class QueryRepository : IQueryRepository
     {
 
-        //public IList<Metadata> Query()
-        //public normalise(YamlDocument yamlDocument)
-        //{
-        //    var yaml_doc = new StreamReader("C: /Users/U6072725/Desktop/main/config.yaml");
-        //    var yaml = new YamlStream();
-        //    var yml_doc = yaml.Load(reader);
-        //    foreach(KeyValuePair keyValue in yaml)
-        //    {
-        //    }
-        //}
-        //}
         private readonly IElasticClientFactory _clientFactory;
         public QueryRepository(IElasticClientFactory clientFactory)
         {
@@ -35,45 +24,20 @@ namespace ElasticSearchDemoApp.Infrastructure
             string filepath = "config.json";
             var a = File.ReadAllText(@"Configuration\\" + filepath);
             var root = JsonConvert.DeserializeObject<JsonObject>(a);
-            //string readResult = string.Empty;
-            //string writeResult = string.Empty;
-            //int normalise_factor = 10;
             var fields = new List<string>();
             var  weights = new List<int>();
-            // root.LeftText = root.LeftText / norm;
-
             var dict = JsonConvert.DeserializeObject<Dictionary<string, int>>(a);
             foreach (var kv in dict)
             {
-               // Console.WriteLine(kv.Key + ":" + kv.Value);
                 weights.Add(kv.Value);
                 fields.Add(kv.Key + "^" + kv.Value);
-                //fields.Add(kv.Key);
-                // kv.Value = kv.Value / normalise_factor;
-
             }
-
-            //weights.Add(root.LeftText);
-            //weights.Add(root.BottomText);
-            //weights.Add(root.ControlText);
-            //for(int i =0; i<3; i++)
-            //{
-            //    var normalise_value = weights[i] / 5;
-            //}
-            // root.ControlText.va
             return fields;
         }
         public IList<Metadata> SearchJson()
         {
             var client = _clientFactory.CreateClient();
             string simplified_search="Minnesota";
-
-            //var response = client.Search<Metadata>(s => s
-            //  .Index("metadata1120")
-            //  .Query(q => q
-            //  .Match(m => m.Field(LoadJson().First()).Query(simplified_search)
-            //      )));
-          //  Console.WriteLine(LoadJson().ToString());
 
             var response = client.Search<Metadata>(s => s
             .Index("metadata1120")
